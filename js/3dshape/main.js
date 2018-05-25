505249922cf7899537b48ebef8273e03b4f2569e
Brookfeild <37666917+Brookfeild@users.noreply.github.com>
2018-05-25 13:30:42 -0400

    Update main.js

--- js/3dshape/main.js
+++ js/3dshape/main.js
@@ -1,11 +1,11 @@
 function setup() {
-  createCanvas(400, 400,WEBGL);
+  createCanvas(1000, 1000,WEBGL);
 }
 
 function draw() {
   background(220);
   rotateX(frameCount = -mouseY * 0.02);
-  rotateY(frameCount = -mouseX * 0.01);
+  rotateY(frameCount = mouseX * 0.01);
   fill("blue");
-  cone(100,100,24,16);
+  cone(400,400,24,16);
 }
