571e413d247b34ac632bfbf1e987d5851b85d8c1
tyler <tylerjricketts@gmail.com>
2025-06-14 03:34:46 -0700

    push

--- ravi/js/include-header.js
+++ ravi/js/include-header.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/partials/header.html')
+    fetch('/ravi/partials/header.html') // use /ravi/ here
       .then(res => res.text())
       .then(html => {
         document.getElementById('site-header').innerHTML = html;
       })
-      .catch(err => console.error('Failed to load header:', err));
+      .catch(err => {
+        console.error('Header load failed:', err);
+      });
   });
-  
